e6e97e3b269b50c05998d87b05205af9ea8b3c49
DanilaSokolov <sokolovdanila2000@gmail.com>
2023-05-17 11:24:57 +0300

    Added gradient dots

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles.js";
+import { ComputersCanvas } from "./canvas"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="navbarSection">
+      <div className={`${styles.paddingX} textSection`}>
+        <div className="decoration">
+            <div className="dot_begin"/>
+            <div className="dot_end violet-gradient"/>
+        </div>
+      </div>
+    </section>
   )
 }
 
