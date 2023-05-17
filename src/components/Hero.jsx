66bfe12cea418610a04337c102d2a477e1a2ec33
DanilaSokolov <sokolovdanila2000@gmail.com>
2023-05-17 11:41:52 +0300

    Added text as a short description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,12 @@ const Hero = () => {
             <div className="dot_begin"/>
             <div className="dot_end violet-gradient"/>
         </div>
+        <div className="text">
+            <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span>Danila</span></h1>
+            <p className={`${styles.heroSubText} subtext`}>
+                I develop web application, console application <br/> and working with databases, API
+            </p>
+        </div>
       </div>
     </section>
   )
