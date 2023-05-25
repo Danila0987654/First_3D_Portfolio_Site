a932453be6501e1235ed71461ff7c650a6c278a7
DanilaSokolov <sokolovdanila2000@gmail.com>
2023-05-25 10:05:16 +0300

    Added header for this section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles.js";
+import { services} from "../constants/index.js";
+import { fadeIn, textVariant } from "../utils/motion.js";
 
 const About = () => {
   return (
-    <div>About</div>
+      <>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introducing</p>
+          <h2 className={styles.sectionHeadText}>Overview.</h2>
+        </motion.div>
+      </>
   )
 }
 
